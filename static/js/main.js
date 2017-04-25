94b01998bf53e5a345cc4d8097e9306ca8b3688f
vitalii_denysiuk <dendoc97@gmail.com>
2017-04-26 00:20:11 +0300

    section home

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,29 @@
 var app = angular.module("KETapp", ['smoothScroll']);
+
+var width = window.innerWidth;
+
 app.controller('headerCtrl', ['$scope', function ($scope) {
     $scope.itemsMenu = ['home', 'services', 'portfolio', 'about', 'news', 'contact'];
     $scope.isClick = false;
-    var width = window.innerWidth;
+
     $scope.removeActive = function (link) {
-      var firstLink = document.querySelector("#menu-list li:first-child a");
+      $scope.firstLink = document.querySelector("#menu-list li:first-child a");
       if (width > 768) {
         if (link !== $scope.itemsMenu[0]) {
           if (!$scope.isClick) {
-            firstLink.classList.add('noborder');
+            $scope.firstLink.classList.add('noborder');
             $scope.isClick = true;
           }
         } else {
-          firstLink.classList.remove('noborder');
+          $scope.firstLink.classList.remove('noborder');
           $scope.isClick = false;
         }
       }
     }
+
     $scope.showMenu = function() {
       var mobileMenu = document.querySelector("#menu-list");
       mobileMenu.classList.toggle('show-menu');
     }
+
 }]);
