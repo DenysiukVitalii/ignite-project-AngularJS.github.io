672ef0f34f3d6621c2f46ed447d4c909d87d005c
vitalii_denysiuk <dendoc97@gmail.com>
2017-05-21 16:47:35 +0300

    add scroll

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
-angular.module("KETapp", ['myHeader', 'ourServices', 'portfolio', 'achievements',
-                          'about', 'ourNews', 'contactUs', 'myFooter']);
+angular.module("KETapp", ['duScroll', 'myHeader', 'ourServices', 'portfolio', 'achievements',
+                          'about', 'ourNews', 'contactUs', 'myFooter']).value("duScrollDuration", 1000);
